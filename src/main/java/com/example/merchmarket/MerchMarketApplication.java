43d54a645a9b465c170ff28c5080d68acdced96c
seog-yeongeol <cheoni5417@naver.com>
2026-02-23 18:38:54 +0900

    git init

---
+++ src/main/java/com/example/merchmarket/MerchMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.merchmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MerchMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/merchmarket/MerchMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.merchmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
